3c3d7e3153e3822f08d5ee0f8f07314261793bd6
chaNcharge <chancharge@gmail.com>
2023-07-27 20:51:22 -0700

    Change to form so pressing enter adds todo

--- components/AddTodo.tsx
+++ components/AddTodo.tsx
@@ -1,18 +1,19 @@
 import { useState } from 'react';
 
-export default function AddTodo({ onAddTodo }) {
+export default function AddTodo({ onAddTodo }: { onAddTodo: (title: string) => void }) {
     const [title, setTitle] = useState('');
     return (
-        <>
+        <form onSubmit={e => {
+            e.preventDefault();
+            setTitle('');
+            onAddTodo(title);
+        }}>
             <input
                 placeholder="Add todo"
                 value={title}
                 onChange={e => setTitle(e.target.value)}
             />
-            <button onClick={() => {
-                setTitle('');
-                onAddTodo(title);
-            }}>Add</button>
-        </>
+            <button type='submit'>Add</button>
+        </form>
     )
 }
